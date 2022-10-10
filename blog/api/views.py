8888a38c7eff0e852b8fd67580442125336b10d7
prosper <edwardprosper001@gmail.com>
2022-10-10 21:00:04 +0100

    code

--- blog/api/views.py
+++ blog/api/views.py
@@ -6,3 +6,23 @@ from .serializers import PostSerializer
 from blog.models import Post
 
 
+@api_view(["GET"])
+def api_list_view(request):
+    if request.method == "GET":
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+
+@api_view(["GET"])
+def api_detail_view(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = PostSerializer(post)
+        
+
+
