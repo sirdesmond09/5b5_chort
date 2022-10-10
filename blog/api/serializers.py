abfe4f0ad4ac05535aaabde97d73bca63c09e988
prosper <edwardprosper001@gmail.com>
2022-10-10 22:19:34 +0100

    code

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from blog.models import Post
+from django.contrib.auth.models import User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,3 +8,12 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
 
         fields = ["title", "content", "date_posted", "slug"]
+
+
+class UserSerialier(serializers.ModelSerializer):
+    class Meta:
+        model = User
+
+        fields = ["pk", "username", "email"]
+
+
