88413ecc2cdac3996a0edb8ae6767b1cf8a1f66c
prosper <edwardprosper001@gmail.com>
2022-10-10 21:09:54 +0100

    code

--- blog/api/views.py
+++ blog/api/views.py
@@ -23,6 +23,44 @@ def api_detail_view(request, slug):
 
     if request.method == "GET":
         serializer = PostSerializer(post)
-        
+        return Response(serializer.data)
+
+
+@api_view(["PUT"])
+def api_update_view(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "PUT":
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            data = {}
+            serializer.save()
+            data["Success"] = "Blog updated successfully"
+            return Response(data=data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["DELETE"])
+def api_delete_view(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "DELETE":
+        data = {}
+        operation = post.delete()
+        if operation:
+            data["Success"] = "Post delete successful"
+        else:
+            data["Failure"] = "Post delete failed"
+        return Response(data=data)
+
+
+
+
 
 
