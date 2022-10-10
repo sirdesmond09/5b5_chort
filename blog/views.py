f81855b680c44671f7f1fe21fbc5b883769ae59f
prosper <edwardprosper001@gmail.com>
2022-10-10 20:41:43 +0100

    code

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,9 @@ def home(request):
     posts = Post.objects.all()
 
     return render(request, "blog/index.html", {"posts": posts})
+
+
+def detail(request, slug):
+    post = Post.objects.get(slug=slug)
+
+    return render(request, "blog/detail.html", {"post": post})
