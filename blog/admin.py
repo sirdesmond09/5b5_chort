f9c4ac25b01a4239bf1142f82340056d150c7d9d
prosper <edwardprosper001@gmail.com>
2022-10-10 20:36:19 +0100

    code

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from .models import Post
 
 # Register your models here.
 
-@admin.registewr(Post)
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ["title", "content", "slug"]
     prepopulated_fields = {'slug': ('title',)}
