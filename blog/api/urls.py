977fdbe7213af93b407aafebc03aa64e656e7286
prosper <edwardprosper001@gmail.com>
2022-10-10 21:22:56 +0100

    code

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 urlpatterns = [
     path("", api_list_view, name="home2"),
     path("<slug:slug>/", api_detail_view, name="detail2"),
-    path("<slug:slug>/update/", api_update_view, name="update"),
-    path("<slug:slug>/delete/", api_delete_view, name="delete"),
+    path("<slug:slug>/update", api_update_view, name="update"),
+    path("<slug:slug>/delete", api_delete_view, name="delete"),
     path("create/", api_create_view, name="create")
 ]
